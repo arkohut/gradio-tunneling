c66f4ecfd6e9f898d8d38f2ad7e5f2f33e224fdd
arkohut <39525455+arkohut@users.noreply.github.com>
2024-06-17 15:56:24 +0800

    chore: bump 0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradio-tunneling',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     package_data={
         'gradio_tunneling': ['frpc*'],
